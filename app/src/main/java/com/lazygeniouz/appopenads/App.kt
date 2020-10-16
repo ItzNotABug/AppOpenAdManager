1c11a954edacd4fa1640605f9084472440810d18
DarShan Pandya <itznotabug@gmail.com>
2020-10-16 19:39:52 +0530

    add proper comments for Codacy.

--- app/src/main/java/com/lazygeniouz/appopenads/App.kt
+++ app/src/main/java/com/lazygeniouz/appopenads/App.kt
@@ -1,14 +1,16 @@
+@file:Suppress("unused")
+
 package com.lazygeniouz.appopenads
 
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import com.lazygeniouz.aoa.AppOpenManager
 
+/** Sample App's Main Application */
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         MobileAds.initialize(this)
         AppOpenManager(this)
     }
--- app/src/main/java/com/lazygeniouz/appopenads/MainActivity.kt
+++ app/src/main/java/com/lazygeniouz/appopenads/MainActivity.kt
@@ -2,8 +2,8 @@ package com.lazygeniouz.appopenads
 
 import android.app.Activity
 import android.os.Bundle
-import com.lazygeniouz.appopenads.R
 
+/** Sample App's Main Activity */
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
