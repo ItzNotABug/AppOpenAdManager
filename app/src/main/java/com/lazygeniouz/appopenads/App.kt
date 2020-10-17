079490549169ac8b14657e102dda15e08fe0381d
DarShan Pandya <itznotabug@gmail.com>
2020-10-17 13:17:52 +0530

    Add initial delay for testing.

--- app/src/main/java/com/lazygeniouz/appopenads/App.kt
+++ app/src/main/java/com/lazygeniouz/appopenads/App.kt
@@ -5,6 +5,8 @@ package com.lazygeniouz.appopenads
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import com.lazygeniouz.aoa.AppOpenManager
+import com.lazygeniouz.aoa.idelay.DelayType
+import com.lazygeniouz.aoa.idelay.InitialDelay
 
 /** Sample App's Main Application */
 class App : Application() {
@@ -12,6 +14,8 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         MobileAds.initialize(this)
-        AppOpenManager(this)
+
+        //InitialDelay to ZERO for testing
+        AppOpenManager(this, InitialDelay(1, DelayType.HOUR))
     }
 }
