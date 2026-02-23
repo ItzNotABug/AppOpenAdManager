77d7f64b0cf105fe43d168b8605db797d4f13629
Darshan <itznotabug@gmail.com>
2026-02-23 14:55:01 +0530

    update: sample app.

--- app/src/main/java/com/lazygeniouz/appopenads/App.kt
+++ app/src/main/java/com/lazygeniouz/appopenads/App.kt
@@ -3,32 +3,44 @@
 package com.lazygeniouz.appopenads
 
 import android.app.Application
+import android.util.Log
 import com.google.android.gms.ads.MobileAds
+import com.lazygeniouz.aoa.AppOpenAdManager
 import com.lazygeniouz.aoa.configs.Configs
 import com.lazygeniouz.aoa.extensions.getAppOpenAdManager
 import com.lazygeniouz.aoa.idelay.InitialDelay
-import com.lazygeniouz.aoa.listener.AppOpenAdListener
 import com.lazygeniouz.appopenads.activities.SplashActivity
 
 /**
  * Sample App's Main Application
  * Kotlin Version, registered in the Manifest.
- * */
-class AppKt : Application() {
+ */
+class App : Application() {
+
+    lateinit var adManager: AppOpenAdManager
+        private set
 
     override fun onCreate() {
         super.onCreate()
 
         MobileAds.initialize(this)
-        getAppOpenAdManager(
+
+        adManager = getAppOpenAdManager(
             Configs(
-                InitialDelay.NONE,
+                initialDelay = InitialDelay.NONE,
                 showInActivities = arrayListOf(SplashActivity::class.java),
             )
-        ).setAppOpenAdListener(object : AppOpenAdListener() {
-            override fun onAdShown() = println("AppOpenAdListener#onAdShown")
-            override fun onAdLoaded() = println("AppOpenAdListener#onAdLoaded")
-            override fun onAdWillShow() = println("AppOpenAdListener#onAdWillShow")
-        }).loadAppOpenAd()
+        ).apply {
+            setOnPaidEventListener { adValue ->
+                val revenue = adValue.valueMicros / 1_000_000.0
+                Log.d(TAG, "Ad earned: $revenue ${adValue.currencyCode}")
+            }
+
+            loadAppOpenAd()
+        }
+    }
+
+    companion object {
+        private const val TAG = "AppOpenAds"
     }
 }
